dd5913deb1f736d0b0b9da6bacbeb8683f82d3fb
MarijaaTrajkovic <trajkoviccmarija@gmail.com>
2022-12-29 14:55:01 +0100

    Kod aplikacije

---
+++ WindowsFormsTest/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            //Prikazuje open file dijalog.
+            //Ako korisnik klikne OK ucitava sliku koju je korisnik izabrao
+            if (openFileDialog1.ShowDialog()== DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            //brise sliku iz prikaza
+            pictureBox1.Image = null;
+        }
+
+        private void backgroundButton_Click(object sender, EventArgs e)
+        {
+            //Menja boju dialog box-a na onu koju je izabrao korisnik
+            if(colorDialog1.ShowDialog()==DialogResult.OK)
+                pictureBox1.BackColor= colorDialog1.Color;
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            // Proširuje sliku na velicinu celog picture box-a
+            if(checkBox1.Checked) 
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+            }
+        }
+    }
+}
